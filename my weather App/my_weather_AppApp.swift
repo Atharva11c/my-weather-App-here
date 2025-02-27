46ce1ba7cdfec2af9de17fe2c9d8179db8876a09
Atharva11c <lavhateatharva60@gmail.com>
2025-02-27 20:29:20 +0530

    Initial Commit

---
+++ my weather App/my_weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_weather_AppApp.swift
+//  my weather App
+//
+//  Created by Atharva Lavhate on 27/02/25.
+//
+
+import SwiftUI
+
+@main
+struct my_weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
